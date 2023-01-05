54fed7f14c54f39e1ba5c3ad5200e3982622ea10
Nale <nthn.lemale@gmail.com>
2023-01-05 15:45:31 +0100

    fix(main): Fix discord installer command bc not working anymore

--- DiscordInstaller.sh
+++ DiscordInstaller.sh
@@ -4,4 +4,5 @@ rm -rf Desktop/discordln
 sudo rm -rf /usr/lib64/discord
 tar -xvf "$1"
 sudo mv Discord /usr/lib64/discord
-ln -s /usr/lib64/discord/Discord Desktop/discordln
+sudo rm /usr/bin/discord
+sudo ln -s /usr/lib64/discord/Discord /usr/bin/discord

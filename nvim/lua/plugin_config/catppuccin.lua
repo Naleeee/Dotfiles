0e746ff02aec07961d54800eda1a2a5a91a2112a
Nale <nthn.lemale@gmail.com>
2023-04-08 19:21:41 +0200

    feat(nvim): Add new plugins for indent closetag and indent, create filetype folder with file specific config

--- nvim/lua/plugin_config/catppuccin.lua
+++ nvim/lua/plugin_config/catppuccin.lua
@@ -1,41 +1,57 @@
-return function()
-    require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        --    background = { -- :h background
-        --        light = "latte",
-        --        dark = "mocha",
-        --    },
-        --    compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-        transparent_background = true,
-        --    term_colors = false,
-        --    dim_inactive = {
-        --        enabled = false,
-        --        shade = "dark",
-        --        percentage = 0.15,
-        --    },
-        styles = {
-            comments = { "italic" },
-            conditionals = {},
-            loops = {},
-            functions = { "bold" },
-            keywords = {},
-            strings = {},
-            variables = {},
-            numbers = { "bold" },
-            booleans = {},
-            properties = {},
-            types = {},
-            operators = {},
-        },
-        --    color_overrides = {},
-        --    custom_highlights = {},
-        --    integrations = {
-        --        cmp = true,
-        --        gitsigns = true,
-        --        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        --    },
-    })
+local M = {}
+
+local catppuccin = require('catppuccin')
+
+function M.setup()
+  catppuccin.setup({
+    flavour = 'mocha',
+    styles = {
+      comments = { 'italic' },
+      conditionals = { 'italic' },
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+    },
+    integrations = {
+      aerial = true,
+      cmp = true,
+      dap = {
+        enabled = true,
+        enable_ui = true,
+      },
+      fidget = true,
+      gitsigns = true,
+      illuminate = true,
+      indent_blankline = {
+        enabled = true,
+        colored_indent_levels = false,
+      },
+      leap = true,
+      lsp_trouble = true,
+      markdown = true,
+      navic = {
+        enabled = true,
+        custom_bg = 'NONE',
+      },
+      native_lsp = {
+        enabled = true,
+      },
+      notify = true,
+      nvimtree = true,
+      telescope = true,
+      treesitter = true,
+      which_key = true,
+    },
+  })
+
+  vim.cmd.colorscheme('catppuccin')
 end
+
+return M

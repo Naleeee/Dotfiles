a51419c4393024500bce7eb67473409a50caf5f1
Nale <nthn.lemale@gmail.com>
2023-04-13 15:13:59 +0200

    update(nvim): Add some config for nvim-tree

--- nvim/lua/plugin_config/nvim-tree.lua
+++ nvim/lua/plugin_config/nvim-tree.lua
@@ -1,7 +1,25 @@
 return function()
-    local map = require("utils").map
+  local map = require("utils").map
 
-    require("nvim-tree").setup()
+  -- disable netrw at the very start of your init.lua (strongly advised)
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
 
-    map("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>")
+  -- set termguicolors to enable highlight groups
+  vim.opt.termguicolors = true
+
+  require("nvim-tree").setup({
+    hijack_cursor = false,
+    modified = {
+      enable = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+    git = {
+      ignore = false,
+    },
+  });
+
+  map("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>")
 end

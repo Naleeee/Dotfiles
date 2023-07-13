2d05c9c5c94d908649c0330b5b4a54c3926c8a42
Nale <nthn.lemale@gmail.com>
2023-07-13 19:39:54 +0200

    feat(nvim): Formatting on save is now working for tsserver

--- nvim/lua/plugin_config/nvim-lspconfig.lua
+++ nvim/lua/plugin_config/nvim-lspconfig.lua
@@ -139,7 +139,17 @@ return function()
       if client.supports_method "textDocument/rename" then
         buf_set_keymap("n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>")
       end
+
+      if client.supports_method "textDocument/formatting" then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          desc = "Enable formatting on save",
+          pattern = "*",
+          group = vim.api.nvim_create_augroup("format_on_save", { clear = true }),
+          command = "lua vim.lsp.buf.format()",
+        })
+      end
     end,
+
     filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
     cmd = { "typescript-language-server", "--stdio" },
     capabilities = capabilities,

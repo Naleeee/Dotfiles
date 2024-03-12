4191b1c4c4b49e1f43e18885da4751293069a02f
Nale <nthn.lemale@gmail.com>
2024-03-12 17:22:16 +0900

    bug(nvim): Fix glitched icons on debian

--- nvim/lua/plugin_config/noice.lua
+++ nvim/lua/plugin_config/noice.lua
@@ -11,12 +11,12 @@ return function()
           -- opts: any options passed to the view
           -- icon_hl_group: optional hl_group for the icon
           -- title: set to anything or empty string to hide
-          cmdline = { pattern = "^:", icon = "", lang = "vim" },
-          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+          cmdline = { pattern = "^:", icon = "❱", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = "🔍", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "🔍", lang = "regex" },
           filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
           lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-          help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = "❔" },
           input = {}, -- Used by input()
           -- lua = false, -- to disable a format, set to `false`
         },

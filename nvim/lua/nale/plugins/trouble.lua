ab0d791ecdd63b61c0865b4b5f69ae72c4da0c01
Nale <nthn.lemale@gmail.com>
2024-06-03 09:54:20 +0900

    update(nvim): Update trouble config to new api usage

--- nvim/lua/nale/plugins/trouble.lua
+++ nvim/lua/nale/plugins/trouble.lua
@@ -1,11 +1,13 @@
 return {
 	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-		{ "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Toggle Trouble list" },
+		-- { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
+		-- { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
+		{ "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Open Trouble quickfix list" },
+		{ "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Open Trouble location list" },
 	},
 }

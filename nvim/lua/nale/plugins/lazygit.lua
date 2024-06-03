79212186d20e724673276f62229443991ec7f843
Nale <nthn.lemale@gmail.com>
2024-06-03 09:53:15 +0900

    update(nvim): Update keybinds to more useful ones

--- nvim/lua/nale/plugins/lazygit.lua
+++ nvim/lua/nale/plugins/lazygit.lua
@@ -14,6 +14,6 @@ return {
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>gi", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+		{ "<leader>li", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
 	},
 }

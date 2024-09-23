edce105b1dacebf6af39d3004f84cfd5912d2192
Nale <nthn.lemale@gmail.com>
2024-09-23 13:54:00 +0200

    update(doc): Update nvim doc with more accurate visuals and infos

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
 vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
 vim.opt_local.spell = true
 vim.opt_local.linebreak = true

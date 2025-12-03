d9b6c1a185ccf3934cfd1cca5f5899eda6475b5c
Nathan LEMALE <nathanlemale@MBP-de-Nathan.localdomain>
2025-12-03 13:40:16 +0100

    DOT-dev :recycle: Remove unused tailwind plugin

--- .config/nvim/lua/nale/plugins/nvim-cmp.lua
+++ .config/nvim/lua/nale/plugins/nvim-cmp.lua
@@ -15,7 +15,6 @@ return {
 		},
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
-		"tailwind-tools",
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
 	config = function()
@@ -90,26 +89,6 @@ return {
 				{ name = "path" }, -- file system paths
 				{ name = "bulma", option = { filetypes = { "vue" } } }, -- file system paths
 			}),
-
-			-- configure lspkind for vs-code like pictograms in completion menu
-
-			formatting = {
-				format = lspkind.cmp_format({
-					before = require("tailwind-tools").lspkind_format,
-					mode = "symbol", -- show only symbol annotations
-					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-					-- can also be a function to dynamically calculate max width such as
-					-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-					-- The function below will be called before any actual modifications from lspkind
-					-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-					-- before = function(entry, vim_item)
-					-- 	return vim_item
-					-- end,
-				}),
-			},
 		})
 	end,
 }

8418967d513c872285530d9dab110eb54b56d0d2
Nathan LEMALE <nthn.lemale@gmail.com>
2025-12-05 14:02:28 +0100

    DOT-dev :wrench: Remove duplicated filetype setup

--- .config/nvim/lua/nale/plugins/colorizer.lua
+++ .config/nvim/lua/nale/plugins/colorizer.lua
@@ -2,12 +2,7 @@ return {
 	"norcalli/nvim-colorizer.lua",
 	config = function()
 		require("colorizer").setup({
-			"css",
-			"javascript",
-			"typescript",
-			"html",
-			"lua",
-			"*", -- enable for all files or specify your filetypes
+			"*",
 		}, {
 			RGB = true, -- highlight rgb() colors
 			RRGGBB = true, -- highlight #RRGGBB hex colors

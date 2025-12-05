0fd7e261b018f40871b90163b3c2b9dcc819e07e
Nathan LEMALE <nthn.lemale@gmail.com>
2025-12-05 14:03:20 +0100

    DOT-Dev :sparkles: Update copilot config to only display NES, no more cmp options

--- .config/nvim/lua/nale/plugins/copilot.lua
+++ .config/nvim/lua/nale/plugins/copilot.lua
@@ -1,28 +1,19 @@
 return {
 	{
-
-		"github/copilot.vim",
-		-- config = function()
-		-- 	local copilot = require("github/copilot.vim")
-		-- 	copilot.setup()
-		-- end,
-	},
-	{
 		"zbirenbaum/copilot.lua",
-		cmd = "Copilot", -- lazy load on command
-		event = "InsertEnter", -- or other lazy trigger
+		cmd = "Copilot",
+		build = ":Copilot auth",
 		config = function()
 			require("copilot").setup({
-				suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "<C-l>" } },
-				panel = { enabled = true, keymap = { jump_next = "<C-j>", jump_prev = "<C-k>", open = "<C-p>" } },
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					keymap = {
+						accept = "<Tab>",
+					},
+				},
+				panel = { enabled = false },
 			})
-		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		dependencies = { "zbirenbaum/copilot.lua", "hrsh7th/nvim-cmp" },
-		config = function()
-			require("copilot_cmp").setup()
 		end,
 	},
 }

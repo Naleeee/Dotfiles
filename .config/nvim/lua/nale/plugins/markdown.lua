958b1cd4daa2efa33523df9a63358f501fc3674a
Nathan LEMALE <nthn.lemale@gmail.com>
2025-12-08 16:55:12 +0100

    DOT-Dev :sparkles: Add markdown inline formatter

--- .config/nvim/lua/nale/plugins/markdown.lua
+++ .config/nvim/lua/nale/plugins/markdown.lua
@@ -5,4 +5,8 @@ return {
 	"dhruvasagar/vim-table-mode",
 	-- Table of content creator in Markdown
 	"mzlogin/vim-markdown-toc",
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	},
 }

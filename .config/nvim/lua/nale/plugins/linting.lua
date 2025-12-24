10eb84cf70f0dabe3003f0112485e482d01fe28f
Nathan LEMALE <nthn.lemale@gmail.com>
2025-12-24 16:08:36 +0100

    DOT-223 :sparkles: Upgrade markdown config

--- .config/nvim/lua/nale/plugins/linting.lua
+++ .config/nvim/lua/nale/plugins/linting.lua
@@ -14,6 +14,26 @@ return {
 			markdown = { "markdownlint" },
 		}
 
+		-- Configure markdownlint to disable certain rules
+		-- Use vim.list_extend to prepend args while keeping the defaults
+		local markdownlint = lint.linters.markdownlint
+		local original_args = markdownlint.args
+		markdownlint.args = function()
+			local args = {
+				"--disable",
+				"MD013", -- line length
+				"MD033", -- no inline HTML
+				"MD041", -- first line h1
+				"--",
+			}
+			-- Append original args (handles both table and function)
+			local orig = type(original_args) == "function" and original_args() or original_args
+			for _, arg in ipairs(orig or {}) do
+				table.insert(args, arg)
+			end
+			return args
+		end
+
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
--- .config/nvim/lua/nale/plugins/markdown-preview.lua
+++ .config/nvim/lua/nale/plugins/markdown-preview.lua
@@ -6,18 +6,37 @@ return {
 	init = function()
 		vim.g.strip_whitespace_on_save = 1
 
-		vim.g.mkdp_auto_start = 0 -- go to browser when opening a .md
+		-- Browser settings - use system default browser
+		vim.g.mkdp_browser = "" -- empty string = system default browser
+		vim.g.mkdp_open_to_the_world = 0
+		vim.g.mkdp_open_ip = ""
+		vim.g.mkdp_port = ""
 
-		vim.g.mkdp_auto_close = 1 -- close browser when leaving a .md
+		vim.g.mkdp_auto_start = 0 -- don't auto-open browser when opening .md
+		vim.g.mkdp_auto_close = 1 -- close browser tab when leaving .md
+		vim.g.mkdp_refresh_slow = 0 -- refresh on every change (set to 1 for only on save)
+		vim.g.mkdp_command_for_global = 0 -- only available in markdown files
 
-		vim.g.mkdp_refresh_slow = 1 -- refreshes automatically the browser window
+		vim.g.mkdp_page_title = "${name} - Markdown Preview"
+		vim.g.mkdp_filetypes = { "markdown" }
 
-		vim.g.mkdp_page_title = "「${name}」-- Vim Markdown" -- give a name to window
+		-- Preview options
+		vim.g.mkdp_preview_options = {
+			mkit = {},
+			katex = {},
+			uml = {},
+			maid = {},
+			disable_sync_scroll = 0,
+			sync_scroll_type = "middle",
+			hide_yaml_meta = 1,
+			sequence_diagrams = {},
+			flowchart_diagrams = {},
+			content_editable = false,
+			disable_filename = 0,
+			toc = {},
+		}
 
-		vim.g.mkdp_filetypes = { "markdown", "md" }
-		-- set keymaps
-		local keymap = vim.keymap
-
-		keymap.set("n", "<C-p>", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Open file in browser preview" })
+		-- Keymap for preview toggle
+		vim.keymap.set("n", "<C-p>", "<cmd>MarkdownPreview<cr>", { desc = "Open markdown preview in browser" })
 	end,
 }

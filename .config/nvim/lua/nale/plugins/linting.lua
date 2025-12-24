8eb8a28efa9dea5f3fc724e4c37ac4ff8cd16881
Nathan LEMALE <nthn.lemale@gmail.com>
2025-12-24 15:48:22 +0100

    DOT-223 :wrench: Rework web formatting to manage auto tool detection

--- .config/nvim/lua/nale/plugins/linting.lua
+++ .config/nvim/lua/nale/plugins/linting.lua
@@ -4,15 +4,14 @@ return {
 	config = function()
 		local lint = require("lint")
 
+		-- NOTE: JavaScript/TypeScript linting is handled by Biome LSP and ESLint LSP
+		-- They auto-detect their config files (biome.json, .eslintrc, etc.)
+		-- Do NOT add biomejs/eslint_d here to avoid duplicate diagnostics
 		lint.linters_by_ft = {
 			-- c = { "clangd" },
 			cmake = { "cmakelint" },
 			-- cpp = { "clangd" },
-			javascript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
 			markdown = { "markdownlint" },
-			typescript = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })

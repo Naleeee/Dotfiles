373823c18b57d2dde1da4063e1c7c3f1c009b8bb
Nathan LEMALE <nthn.lemale@gmail.com>
2025-12-24 16:09:08 +0100

    DOT-223 :sparkles: Create new keybindings for trouble view

--- .config/nvim/lua/nale/plugins/trouble.lua
+++ .config/nvim/lua/nale/plugins/trouble.lua
@@ -1,13 +1,30 @@
 return {
 	"folke/trouble.nvim",
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	opts = {
+		focus = true, -- Focus trouble window when opened
+	},
 	cmd = "Trouble",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Toggle Trouble list" },
-		-- { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-		-- { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-		{ "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Open Trouble quickfix list" },
-		{ "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Open Trouble location list" },
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (workspace)" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Diagnostics (buffer)" },
+		{ "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix list" },
+		{ "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location list" },
+		{ "<leader>xs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols outline" },
+		-- Show diagnostic info in floating window
+		{
+			"<leader>xd",
+			function()
+				vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
+			end,
+			desc = "Show diagnostic (cursor)",
+		},
+		{
+			"<leader>xD",
+			function()
+				vim.diagnostic.open_float(nil, { focus = false, scope = "line" })
+			end,
+			desc = "Show diagnostics (line)",
+		},
 	},
 }

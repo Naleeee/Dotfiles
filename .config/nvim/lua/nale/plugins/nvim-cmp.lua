bd4cfff16c03d5743ec08f1a1e0da347841d5726
Nathan LEMALE <nthn.lemale@gmail.com>
2026-01-21 15:51:26 +0100

    DOT-243 :sparkles: Rework lsp behavior to use lspsaga better ui

--- .config/nvim/lua/nale/plugins/nvim-cmp.lua
+++ .config/nvim/lua/nale/plugins/nvim-cmp.lua
@@ -36,6 +36,11 @@ return {
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
+			experimental = {
+				ghost_text = {
+					hl_group = "Comment",
+				},
+			},
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
 					luasnip.lsp_expand(args.body)

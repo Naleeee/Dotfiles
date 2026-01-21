d417c6a0128d0d230593c6e52647e6bb9f8cc01a
Nathan LEMALE <nthn.lemale@gmail.com>
2026-01-21 15:52:01 +0100

    Revert "Dot 243"

--- .config/nvim/lua/nale/lsp.lua
+++ .config/nvim/lua/nale/lsp.lua
@@ -2,8 +2,7 @@ local keymap = vim.keymap -- for conciseness
 
 -- Config file patterns for linter detection
 local biome_configs = { "biome.json", "biome.jsonc" }
-local eslint_configs =
-	{ ".eslintrc", ".eslintrc.json", ".eslintrc.js", ".eslintrc.cjs", "eslint.config.js", "eslint.config.mjs" }
+local eslint_configs = { ".eslintrc", ".eslintrc.json", ".eslintrc.js", ".eslintrc.cjs", "eslint.config.js", "eslint.config.mjs" }
 
 -- Helper: check if config files exist upward from buffer
 local function has_config(patterns, bufnr)
@@ -40,52 +39,53 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
 		local opts = { buffer = ev.buf, silent = true }
 
-		-- Lspsaga keymaps (prettier UI)
+		-- set keybinds
 		opts.desc = "Show LSP references"
-		keymap.set("n", "gR", "<cmd>Lspsaga finder<CR>", opts)
+		keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-		opts.desc = "Peek definition"
-		keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts)
+		opts.desc = "Go to declaration"
+		keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-		opts.desc = "Go to definition"
-		keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
+		opts.desc = "Show LSP definition"
+		keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definition
 
 		opts.desc = "Show LSP implementations"
-		keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+		keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
 		opts.desc = "Show LSP type definitions"
-		keymap.set("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>", opts)
+		keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-		opts.desc = "Code actions"
-		keymap.set({ "n", "v" }, "<leader>qf", "<cmd>Lspsaga code_action<CR>", opts)
+		opts.desc = "See available quick fixes"
+		keymap.set({ "n", "v" }, "<leader>qf", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
 		opts.desc = "Smart rename"
-		keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+		keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
 		opts.desc = "Show buffer diagnostics"
-		keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+		keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
 		opts.desc = "Show line diagnostics"
-		keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+		keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 		opts.desc = "Go to previous diagnostic"
-		keymap.set("n", "<leader>n", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-
+		keymap.set("n", "[d", function()
+			vim.diagnostic.jump({ count = -1, float = true })
+		end, opts) -- jump to previous diagnostic in buffer
+		--
 		opts.desc = "Go to next diagnostic"
-		keymap.set("n", "<leader>N", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+		keymap.set("n", "]d", function()
+			vim.diagnostic.jump({ count = 1, float = true })
+		end, opts) -- jump to next diagnostic in buffer
 
-		opts.desc = "Show documentation"
-		keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-
-		opts.desc = "Toggle outline"
-		keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<CR>", opts)
+		opts.desc = "Show documentation for what is under cursor"
+		keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
 		opts.desc = "Restart LSP"
-		keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+		keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 	end,
 })
 
-vim.lsp.inlay_hint.enable(true)
+-- vim.lsp.inlay_hint.enable(true)
 
 local severity = vim.diagnostic.severity
 

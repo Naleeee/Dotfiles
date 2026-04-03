1f43bc2528bdf0881807130e6448a52a60e48364
Nathan LEMALE <nthn.lemale@gmail.com>
2026-04-03 14:33:33 +0200

    DOT-268 :sparkles: Switch to blink cmp instead of nvim cmp for lighter config

--- .config/nvim/lua/nale/plugins/noice.lua
+++ .config/nvim/lua/nale/plugins/noice.lua
@@ -25,7 +25,7 @@ return {
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
+
 				},
 				hover = { enabled = false }, -- Use lspsaga instead
 				signature = { enabled = true },

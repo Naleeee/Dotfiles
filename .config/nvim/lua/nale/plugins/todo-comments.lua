65840f3ef6036964f98bf631c2118e275f3cbe8f
Nathan LEMALE <nthn.lemale@gmail.com>
2026-04-03 10:54:22 +0200

    DOT-268 :fire: Remove useless dependency

--- .config/nvim/lua/nale/plugins/todo-comments.lua
+++ .config/nvim/lua/nale/plugins/todo-comments.lua
@@ -1,6 +1,6 @@
 return {
 	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim", "ibhagwan/fzf-lua" },
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 	config = function()
 		local todo_comments = require("todo-comments")
 

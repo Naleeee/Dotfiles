5214063923403b94c87ffea72433953626408f0e
Nathan LEMALE <nthn.lemale@gmail.com>
2026-05-13 17:50:46 +0200

    POR-277 :sparkles: Switch back to markdown preview

--- .config/nvim/lua/nale/plugins/markdown-preview.lua
+++ .config/nvim/lua/nale/plugins/markdown-preview.lua
@@ -1,24 +1,13 @@
 return {
-	"toppair/peek.nvim",
-	event = { "VeryLazy" },
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
-	build = "deno task --quiet build:fast",
+	build = "cd app && npx --yes yarn install",
 	config = function()
-		require("peek").setup({
-			auto_load = false,
-			close_on_bdelete = true,
-			syntax = true,
-			theme = "dark",
-			update_on_change = true,
-			app = "browser",
-			filetype = { "markdown" },
-			throttle_at = 200000,
-			throttle_time = "auto",
-		})
+		vim.g.mkdp_auto_start = 0
+		vim.g.mkdp_auto_close = 1
+		vim.g.mkdp_theme = "dark"
 
-		vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-		vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-
-		vim.keymap.set("n", "<C-p>", "<cmd>PeekOpen<cr>", { desc = "Open markdown preview in browser" })
+		vim.keymap.set("n", "<C-p>", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview in browser" })
 	end,
 }
